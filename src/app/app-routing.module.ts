0fb08ad4e94c8bd7174915388afda19dc7365394
Timothe <hegetimothe@gmail.com>
2024-02-23 12:36:44 +0100

    corection formulaire inscription, corection du bug des shape en bg, ajout du rooting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { PanierComponent } from './component/panier/panier.component';
+import { ProductPageComponent } from './component/product-page/product-page.component';
+import { SigninComponent } from './component/signin/signin.component';
 import { LoginComponent } from './component/login/login.component';
 
-
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  { path: "home", component: HomeComponent},
+  { path: "panier", component: PanierComponent},
+  { path: "product", component: ProductPageComponent},
+  { path: "signin", component: SigninComponent},
+  { path: "", component: LoginComponent},
 ];
 
 @NgModule({
