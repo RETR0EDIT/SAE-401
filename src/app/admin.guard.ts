4225d0133c47a9ae0fa996571aff48ac684df08f
RETR0EDIT <nolandme2003@gmail.com>
2024-03-09 16:30:53 +0100

    admin box

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth-service.service';
 
 @Injectable({
@@ -11,8 +9,8 @@ export class AdminGuard implements CanActivate {
   constructor(private authService: AuthService) {}
 
   canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
+    _: ActivatedRouteSnapshot,
+    __: RouterStateSnapshot): boolean {
     return this.authService.userRole === 'admin';
   }
 }
