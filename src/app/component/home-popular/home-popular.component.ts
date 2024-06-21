f6a25044a9dc8085dc2302959cee12eaa580d37f
cquoitoutcarabedemerdedu93 <nolandme2003@gmail.com>
2024-06-21 14:18:57 +0200

    fix read to Read

--- src/app/component/home-popular/home-popular.component.ts
+++ src/app/component/home-popular/home-popular.component.ts
@@ -5,20 +5,24 @@ import { Box } from '../../box.interface';
 @Component({
   selector: 'app-home-popular',
   templateUrl: './home-popular.component.html',
-  styleUrls: ['./home-popular.component.scss']
+  styleUrls: ['./home-popular.component.scss'],
 })
 export class HomePopularComponent implements OnInit {
   boxes: Box[] = [];
-  url = 'http://localhost/SAE-401/api/box/read.php';
+  url = 'http://localhost/SAE-401/api/box/Read.php';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get<Box[]>(this.url).subscribe(boxes => {
-      this.boxes = boxes.map(box => ({
-        ...box,
-        saveurs: Array.isArray(box.saveurs) ? box.saveurs.join(', ') : box.saveurs
-      })).slice(0, 4);
+    this.http.get<Box[]>(this.url).subscribe((boxes) => {
+      this.boxes = boxes
+        .map((box) => ({
+          ...box,
+          saveurs: Array.isArray(box.saveurs)
+            ? box.saveurs.join(', ')
+            : box.saveurs,
+        }))
+        .slice(0, 4);
     });
   }
-};
+}
